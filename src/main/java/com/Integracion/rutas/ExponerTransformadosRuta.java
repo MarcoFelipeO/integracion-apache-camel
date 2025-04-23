dea2d41ae164257dbf6d5b5fab240c14794123d7
Marco Orellana <mafeorellana09@gmail.com>
2025-04-22 21:08:00 -0400

    first commit

--- src/main/java/com/Integracion/rutas/ExponerTransformadosRuta.java
+++ src/main/java/com/Integracion/rutas/ExponerTransformadosRuta.java
@@ -10,10 +10,12 @@ public class ExponerTransformadosRuta extends RouteBuilder {
     @Override
     public void configure() throws Exception {
 
-        from("jetty:http://0.0.0.0:8081/usuarios-transformados")
+        from("jetty:http://0.0.0.0:8081/usuarios-transformados?httpMethodRestrict=GET")
                 .routeId("exponer-usuarios-transformados")
+                .setHeader("Access-Control-Allow-Origin", constant("*")) // <-- habilita CORS
                 .setHeader("Content-Type", constant("application/json"))
                 .process(exchange -> exchange.getMessage().setBody(UsuarioMemoria.getUsuarios()))
                 .marshal().json();
+
     }
 }
